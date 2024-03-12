01802b64ef425f951b4265b4fd8f1171237aacc6
Jeffrey Stevens <51374986+JeffreyRStevens@users.noreply.github.com>
2024-03-12 10:26:34 -0500

    Create initial logo

---
+++ logo/logo.R
@@ -0,0 +1,17 @@
+library(hexSticker)
+
+# imgurl <- system.file("cocoon.png", package="hexSticker")
+sticker("logo/cocoon.png", package="cocoon",
+        p_size = 20, #p_color = "#000000",
+        p_y = 0.45,
+        s_x = 1, s_y = 1.1, s_width = 0.5,
+        h_fill = "#a1b979ff", h_color = "#000000",
+        filename="logo/logo.png")
+
+sticker("logo/cocoon_black.png", package="cocoon",
+        p_size = 30, #p_color = "#000000",
+        p_y = 0.55,
+        s_x = 1, s_y = 1.3, s_width = 0.28,
+        h_fill = "#a1b979ff", h_color = "#000000",
+        filename="logo/logo.png")
+
