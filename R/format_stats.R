c253be162cd2d4eb617f52a32c544cf17d8c84ff
Jeffrey R. Stevens <jeffrey.r.stevens@protonmail.com>
2024-10-04 13:05:00 -0500

    Consolidate format_stats.BFBayesFactor

--- R/format_stats.R
+++ R/format_stats.R
@@ -17,7 +17,7 @@
 #' @param x Statistical object.
 #' @param ... Additional arguments passed to methods. For method-specific
 #' arguments, see [format_stats.htest()] for htest correlations, t-tests,
-#' and Wilcoxon tests and [formatstat.BayesFactor()] for Bayes factors from
+#' and Wilcoxon tests and [format_stats.BFBayesFactor()] for Bayes factors from
 #' the \{[BayesFactor](https://cran.r-project.org/package=BayesFactor)\} package.
 #'
 #' @return
@@ -171,13 +171,56 @@ format_stats.htest <- function(x,
 
 #' Format Bayes factors
 #'
+#' This method formats Bayes factors from the
+#' \{[BayesFactor](https://cran.r-project.org/package=BayesFactor)\} package.
+#' By default, this function rounds Bayes factors greater than 1 to one decimal
+#' place and Bayes factors less than 1 to two decimal places. Values greater
+#' than 1000 or less than 1/1000 are formatted using scientific notation.
+#' Cutoffs can be set that format the values as greater than or less than the
+#' cutoffs (e.g., BF > 1000 or BF < 0.001). Numbers of digits, cutoffs,
+#' italics, and label subscripts are all customizable.
+#'
+#' @param x BayesFactor object or vector of numeric Bayes factor values
+#' @param digits1 Number of digits after the decimal for Bayes factors > 1
+#' @param digits2 Number of digits after the decimal for Bayes factors < 1
+#' @param cutoff Cutoff for using `_BF_~10~ > <cutoff>` or
+#' `_BF_~10~ < 1 / <cutoff>` (value must be > 1)
+#' @param label Character string for label before Bayes factor. Default is BF.
+#' Set `label = ""` to return just the formatted Bayes factor value with no
+#' label or operator (`=`, `<`, `>`)
+#' @param italics Logical value (default = TRUE) for whether label should be
+#' italicized (_BF_ or BF)
+#' @param subscript Subscript to include with _BF_ label (`"10"`, `"01"`, or
+#' `""` for no subscript)
+#' @param type Type of formatting (`"md"` = markdown, `"latex"` = LaTeX)
 #'
 #'
 #' @method format_stats BFBayesFactor
 #' @family functions for printing statistical objects
 #' @export
-format_stats.BFBayesFactor <- function(x, ...) {
-  format_bf(x, ...)
+#'
+#' @examples
+#' test_bf <- BayesFactor::ttestBF(mtcars$vs, mtcars$am)
+#' format_stats(test_bf)
+#' format_stats(test_bf, cutoff = 3)
+#' format_stats(test_bf, digits2 = 1, italics = FALSE, subscript = "")
+format_stats.BFBayesFactor <- function(x,
+                                       digits1 = 1,
+                                       digits2 = 2,
+                                       cutoff = NULL,
+                                       label = "BF",
+                                       italics = TRUE,
+                                       subscript = "10",
+                                       type = "md") {
+
+  format_bf(x,
+            digits1 = digits1,
+            digits2 = digits2,
+            cutoff = cutoff,
+            label = label,
+            italics = italics,
+            subscript = subscript,
+            type = type)
 }
 
 
@@ -211,7 +254,13 @@ format_stats.BFBayesFactor <- function(x, ...) {
 #' # format_stats(mtcars_corr, pdigits = 2)
 #' # Add leading zero to p-value and don't print confidence intervals
 #' # format_stats(mtcars_corr, pzero = TRUE, ci = FALSE)
-format_corr <- function(x, digits, pdigits, pzero, full, italics, type) {
+format_corr <- function(x,
+                        digits,
+                        pdigits,
+                        pzero,
+                        full,
+                        italics,
+                        type) {
   # Check arguments
   # stopifnot("Input must be a correlation object." = inherits(x, what = "htest") && grepl("correlation", x$method))
   # stopifnot("Argument `digits` must be a non-negative numeric vector." = is.numeric(digits))
@@ -373,19 +422,7 @@ format_ttest <- function(x,
 #' BF < 0.001). Numbers of digits, cutoffs, italics, and label subscripts are
 #' all customizable.
 #'
-#' @param x BayesFactor object or vector of numeric Bayes factor values
-#' @param digits1 Number of digits after the decimal for Bayes factors > 1
-#' @param digits2 Number of digits after the decimal for Bayes factors < 1
-#' @param cutoff Cutoff for using `_BF_~10~ > <cutoff>` or
-#' `_BF_~10~ < 1 / <cutoff>` (value must be > 1)
-#' @param label Character string for label before Bayes factor. Default is BF.
-#' Set `label = ""` to return just the formatted Bayes factor value with no
-#' label or operator (`=`, `<`, `>`)
-#' @param italics Logical value (default = TRUE) for whether label should be
-#' italicized (_BF_ or BF)
-#' @param subscript Subscript to include with _BF_ label (`"10"`, `"01"`, or
-#' `""` for no subscript)
-#' @param type Type of formatting (`"md"` = markdown, `"latex"` = LaTeX)
+#' @inheritParams format_stats.BFBayesFactor
 #'
 #' @return
 #' A character string that includes label (by default _BF_~10~) and then the
@@ -422,7 +459,7 @@ format_bf <- function(x,
                       italics = TRUE,
                       subscript = "10",
                       type = "md") {
-  # Check arguments
+  # Check input type
   if (is.numeric(x)) {
     bf <- x
   } else if (inherits(x, what = "BFBayesFactor")) {
@@ -430,6 +467,8 @@ format_bf <- function(x,
   } else {
     stop("Input is not numeric or of class BFBayesFactor.")
   }
+
+  # Validate arguments
   stopifnot("Argument `digits1` must be a non-negative numeric vector." = is.numeric(digits1))
   stopifnot("Argument `digits1` must be a non-negative numeric vector." = digits1 >= 0)
   stopifnot("Argument `digits2` must be a non-negative numeric vector." = is.numeric(digits2))
@@ -458,8 +497,10 @@ format_bf <- function(x,
     )
   } else {
     bf_value <- dplyr::case_when(
-      bf >= cutoff ~ as.character(cutoff),
-      bf <= 1 / cutoff ~ as.character(1 / cutoff),
+      bf >= cutoff ~ format_num(cutoff, digits = 0),
+      bf <= 1 / cutoff & format_num(1 / cutoff, digits = digits2) == format_num(0, digits = digits2) ~
+        sub("0$", "1", format_num(1 / cutoff, digits = 3)),
+      bf <= 1 / cutoff ~ format_num(1 / cutoff, digits = digits2),
       bf <= 1 / 10^digits2 ~ as.character(1 / 10^digits2),
       bf >= 1 & bf <= cutoff ~ format_num(bf, digits = digits1),
       bf < 1 & bf >= 1 / cutoff ~ format_num(bf, digits = digits2)
