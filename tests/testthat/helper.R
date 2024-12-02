129a27569c56ed1ad675d61f3b1a43544d5ec6b6
Jeffrey R. Stevens <jeffrey.r.stevens@protonmail.com>
2024-12-02 13:50:22 -0600

    Add format_stats.merMod tests

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -2,7 +2,8 @@ set.seed(2024)
 df <- data.frame(a = 1:10,
                  b = 2:11,
                  c = c(1, 8, 3, 7, 8, 2, 4, 1, 4, 5),
-                 d = sample(0:1, 10, replace = TRUE))
+                 d = sample(0:1, 10, replace = TRUE),
+                 e = rep(1:2, 5))
 test_corr <- cor.test(df$a, df$b)
 test_corr2 <- cor.test(df$a, df$c)
 test_easycorr <- correlation::correlation(df, select = "a", select2 = "c")
@@ -18,6 +19,10 @@ test_chisq <- chisq.test(as.table(rbind(c(762, 327, 468), c(484, 239, 477))))
 test_aov <- aov(c ~ a, data = df)
 test_lm <- lm(c ~ a, data = df)
 test_glm <- glm(d ~ a, data = df, family = binomial)
+test_lmer <- suppressMessages(lme4::lmer(c ~ a + (1 | e), data = df))
+test_glmer <- suppressMessages(lme4::glmer(d ~ a + (1 | e), data = df, family = binomial))
 test_bf <- BayesFactor::ttestBF(df$a, mu = 5)
 
-library(rlang)
+suppressPackageStartupMessages({
+  library(rlang, warn.conflicts = FALSE, quietly = TRUE, verbose = FALSE)
+})
